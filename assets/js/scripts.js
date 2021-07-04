a6622f50f92ecdb9a55f009b82c3b0eddb153ca5
Al-amintpi <alamincmt7418@gmail.com>
2021-07-04 16:02:35 +0600

    Test

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -73,7 +73,41 @@
 			$('.al-header-menu-section').removeClass('fixedMenu');
 		}
 	});
-	
+	// Animation
 	AOS.init();
+
+	$(document).ready(function(){
+
+		$(document).click( function(e) {
+			 $('.translate_wrapper, .more_lang').removeClass('active');
+		});
+		
+		$('.translate_wrapper .current_lang').click(function(e){
+		  e.stopPropagation();
+		  $(this).parent().toggleClass('active');
+		
+		  setTimeout(function(){
+			$('.more_lang').toggleClass('active');
+		  }, 5);
+		});
+		
+		
+		/*TRANSLATE*/
+		
+		$('.more_lang .lang').click(function(){
+		  $(this).addClass('selected').siblings().removeClass('selected');
+		  $('.more_lang').removeClass('active');
+		
+		  var img = $(this).find('img').attr('src');
+		  var lang = $(this).attr('data-value');
+		  var tnum = lang;
+		   
+		
+		  $('.current_lang .lang-txt').text(lang);
+		  $('.current_lang img').attr('src', img);
+		
+		});
+		});
+		
 	  
 })(jQuery);
